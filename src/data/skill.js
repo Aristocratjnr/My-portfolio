cc33411d211e85eca55b6c99330e8625b810aa4b
David Jnr <102563270+Aristocratjnr@users.noreply.github.com>
2024-04-01 15:05:13 +0100

    .

--- src/data/skill.js
+++ src/data/skill.js
@@ -1,13 +1,13 @@
 import { DiAndroid, DiDart, DiNodejs, DiReact } from "react-icons/di";
 
 const data = [
-    { icon: DiAndroid, title: "Android", },
-    { icon: DiDart, title: "Dart", },
+    { icon: DiNextjs, title: "Nextjs", },
+    { icon: DiReact, title: "React ", },
     { icon: DiNodejs, title: "Node ", },
     { icon: DiReact, title: "React Js", },
     { icon: DiAndroid, title: "Android", },
     { icon: DiDart, title: "Dart", },
-    { icon: DiNodejs, title: "Node ", },
+    { icon: DiNodejs, title: "Nodejs", },
     { icon: DiReact, title: "React Js", },
 ];
 
